0f6aa04249f7cdc5f7bb0d4751cdc3b8ae30e36f
Old Liu <446247645@qq.com>
2019-01-14 15:46:08 +0800

    Create 替换空格.py

---
+++ 替换空格.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+#str不能被直接替换
+
+#解1：使用python内建方法append [O(1)]
+#新建一个k，遇到空格append('%20')，其他情况添加原字符。
+class Solution:
+    # s 源字符串
+    def replaceSpace(self, s):
+        # write code here
+        k = []
+        for index,i in enumerate(s):
+            if i == ' ':
+                k.append('%20')
+            else:
+                k.append(i)
+        return "".join(k)
+
+#解2：在原str上修改，建立两个指针，遍历该str
